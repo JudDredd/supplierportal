722f8904855d10927184e1627cd0c5f6ea723b67
Jud <51824158+JudDredd@users.noreply.github.com>
2025-04-26 21:56:38 +1000

    Update FilterBar.jsx

--- components/FilterBar.jsx
+++ components/FilterBar.jsx
@@ -1,38 +1,17 @@
+// File: components/FilterBar.jsx
 import React from "react";
-
 export default function FilterBar({ eventOptions, cityOptions, filters, onFilterChange }) {
   return (
     <div className="flex flex-wrap gap-2 p-4 bg-white shadow sticky top-0 z-10">
-      <select
-        className="p-2 border rounded"
-        value={filters.event}
-        onChange={(e) => onFilterChange({ ...filters, event: e.target.value })}
-      >
+      <select className="p-2 border rounded" value={filters.event} onChange={e => onFilterChange({ ...filters, event: e.target.value })}>
         <option value="">All Events</option>
-        {eventOptions.map((ev) => (
-          <option key={ev.id} value={ev.id}>
-            {ev.name}
-          </option>
-        ))}
+        {eventOptions.map(ev => <option key={ev.id} value={ev.id}>{ev.name}</option>)}
       </select>
-      <select
-        className="p-2 border rounded"
-        value={filters.city}
-        onChange={(e) => onFilterChange({ ...filters, city: e.target.value })}
-      >
+      <select className="p-2 border rounded" value={filters.city} onChange={e => onFilterChange({ ...filters, city: e.target.value })}>
         <option value="">All Cities</option>
-        {cityOptions.map((city) => (
-          <option key={city} value={city}>
-            {city}
-          </option>
-        ))}
+        {cityOptions.map(city => <option key={city} value={city}>{city}</option>)}
       </select>
-      <input
-        type="date"
-        className="p-2 border rounded"
-        value={filters.date}
-        onChange={(e) => onFilterChange({ ...filters, date: e.target.value })}
-      />
+      <input type="date" className="p-2 border rounded" value={filters.date} onChange={e => onFilterChange({ ...filters, date: e.target.value })} />
     </div>
   );
 }
